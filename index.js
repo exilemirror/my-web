2fb7241c9f4b6e8092dec473101724be4ff8205b
exilemirror <exilemirror@yahoo.com.sg>
2020-12-02 12:36:05 +0800

    Added ingress and dockerfile

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    try {
+        res.send('Hello World!');
+      } catch (err) {
+        console.error(err.message);
+        res.status(404).send('Mychat 404 Error');
+      }
 });
+
+// Define Routes
+app.use('/api/session', require('./routes/api/session'));
+app.use("/", (req, res, next) => {
+    res.status("404").json({message: "MyChat 404 Not Found"})
+  })
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server is running on ' + PORT));
