9823ea52ed314edd53c0e799f1bfe5fbadc291e2
exilemirror <exilemirror@yahoo.com.sg>
2020-12-03 16:08:56 +0800

    updated session page

--- routes/api/session.js
+++ routes/api/session.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     try {
-        res.send('Hello Session!');
+        res.send('Hello Updated Session!');
       } catch (err) {
         console.error(err.message);
         res.status(404).send('Mychat 404 Error');
